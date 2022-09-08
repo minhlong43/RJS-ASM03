a629d579354ddb24721f53e6ea50773d9075b30a
DoMinhLong <dominhlong43@gmail.com>
2022-09-08 23:30:01 +0700

    fix error without render newStaff - new error

--- src/components/StaffItemComponent.js
+++ src/components/StaffItemComponent.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import dateFormat from "dateformat";
 
 function StaffDetail(props) {
-  if (props.staff != null) {
+  if (props.staffs != null) {
     return (
       <div className="container">
         <div className="row">
@@ -13,39 +13,70 @@ function StaffDetail(props) {
               <BreadcrumbItem>
                 <Link to="/staff">Nhân viên</Link>
               </BreadcrumbItem>
-              <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
+              <BreadcrumbItem active>{props.staffs.name}</BreadcrumbItem>
             </Breadcrumb>
           </div>
         </div>
-        <div className="row">
-          <StaffItem staff={props.staff} />
+        {/* <div className="row"> */}
+        {/* <StaffItem staff={props.staff} /> */}
+        <div className="container">
+          <div className="col-12">
+            <div className="row mb-5">
+              <div className="col-4">
+                <CardImg
+                  width="100%"
+                  src={props.staffs.image}
+                  alt={props.staffs.name}
+                />
+              </div>
+              <div key={props.staffs.id} className="col-8">
+                <CardBody>
+                  <h4>Họ và tên: {props.staffs.name}</h4>
+                  <p>Ngày sinh: {dateFormat(props.staffs.doB, "dd/mm/yy")}</p>
+                  <p>
+                    Ngày vào công ty:{" "}
+                    {dateFormat(props.staffs.startDate, "dd/mm/yy")}
+                  </p>
+                  <p>Phòng ban: {props.staffs.department.name}</p>
+                  <p>Số ngày nghỉ còn lại: {props.staffs.annualLeave}</p>
+                  <p>Số ngày đã làm thêm: {props.staffs.overTime}</p>
+                </CardBody>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      // </div>
     );
   } else {
     return <div></div>;
   }
 }
 
-function StaffItem(staffs) {
-  if (staffs != null) {
+function StaffItem(props) {
+  if (props.staffs != null) {
     return (
       <div className="container">
         <div className="col-12">
           <div className="row mb-5">
             <div className="col-4">
-              <CardImg width="100%" src={staffs.image} alt={staffs.name} />
+              <CardImg
+                width="100%"
+                src={props.staffs.image}
+                alt={props.staffs.name}
+              />
             </div>
-            <div key={staffs.id} className="col-8">
+            <div key={props.staffs.id} className="col-8">
               <CardBody>
-                <h4>Họ và tên: {staffs.name}</h4>
-                <p>Ngày sinh: {dateFormat(staffs.doB, "dd/mm/yy")}</p>
+                <h4>Họ và tên: {props.staffs.name}</h4>
+                <p>Ngày sinh: {dateFormat(props.staffs.doB, "dd/mm/yy")}</p>
                 <p>
-                  Ngày vào công ty: {dateFormat(staffs.startDate, "dd/mm/yy")}
+                  Ngày vào công ty:{" "}
+                  {dateFormat(props.staffs.startDate, "dd/mm/yy")}
                 </p>
-                <p>Phòng ban: {staffs.department.name}</p>
-                <p>Số ngày nghỉ còn lại: {staffs.annualLeave}</p>
-                <p>Số ngày đã làm thêm: {staffs.overTime}</p>
+                <p>Phòng ban: {props.staffs.department.name}</p>
+                <p>Số ngày nghỉ còn lại: {props.staffs.annualLeave}</p>
+                <p>Số ngày đã làm thêm: {props.staffs.overTime}</p>
               </CardBody>
             </div>
           </div>
